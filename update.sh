71b9394704a01761247e7bc0ef2219b22aa6c12f
lockeysama <196349143@qq.com>
2021-08-04 15:30:15 +0800

    update: 修改数据更新脚本

--- update.sh
+++ update.sh
@@ -1,18 +1,18 @@
 #/bin/sh
 
 UPSTREAM=${1:-'@{u}'}
-REMOTE_RECORD=$(git rev-parse "$UPSTREAM")
+REMOTE_RECORD=$(git ls-remote | grep refs/heads/master | cut -f 1)
 
 while true
 do
-    REMOTE=$(git rev-parse "$UPSTREAM")
+    REMOTE=$(git ls-remote | grep refs/heads/master | cut -f 1)
  
     if [ $REMOTE_RECORD = $REMOTE ]; then
         echo "Up-to-date"
     else
         echo "Need to pull"
         git pull
-        REMOTE_RECORD=$(git rev-parse "$UPSTREAM")
+        REMOTE_RECORD=$(git ls-remote | grep refs/heads/master | cut -f 1)
         cd tDCS
         yarn build
         cd ..
